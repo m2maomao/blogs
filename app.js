4739ca24a86ea5d88f0e040c956a7593e2fb0dcc
michael <m2maomao@163.com>
2025-04-09 22:13:45 +0800

    feat: 初始化路由

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const handleBlogRoute = require('./src/routes/blog');
+console.log('handleBlogRoute: ', handleBlogRoute);
 const serverHandler = (req, res) => {
+  // 设置响应格式
   res.setHeader('Content-Type', 'application/json');
+  
+  const url = req.url;
+  req.path = url.split('?')[0];
 
-  const responseData = {
-    name: 'Michael',
-    age: 18
+  const blogData = handleBlogRoute(req, res);
+  if (blogData) {
+    res.end(
+      JSON.stringify(blogData)
+    )
+    return;
   }
 
-  res.end(
-    JSON.stringify(responseData)
-  )
+  res.writeHead(404, { 'Content-Type': 'text/plain'});
+  res.write('404 Node Found');
+  res.end();
 }
 
 module.exports = serverHandler;
---
+++ src/routes/blog.js
@@ -0,0 +1,34 @@
+// 处理博客相关的路由
+const handleBlogRoute = (req, res) => {
+  // 定义处理路由的逻辑
+  const method = req.method;
+  
+
+  if (method === 'GET' && req.path === '/api/blog/list') {
+    return {
+      message: '获取博客列表的接口'
+    }
+  }
+  if (method === 'GET' && req.path === '/api/blog/detail') {
+    return {
+      message: '获取博客详情的接口'
+    }
+  }
+  if (method === 'POST' && req.path === '/api/blog/new') {
+    return {
+      message: '新建博客的接口'
+    }
+  }
+  if (method === 'POST' && req.path === '/api/blog/update') {
+    return {
+      message: '更新博客的接口'
+    }
+  }
+  if (method === 'POST' && req.path === '/api/blog/delete') {
+    return {
+      message: '删除博客的接口'
+    }
+  }
+}
+
+module.exports = handleBlogRoute;
