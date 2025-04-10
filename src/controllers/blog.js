ebb868105d142f97bfb655a2992e227fd8e40a73
michael <m2maomao@163.com>
2025-04-10 21:22:18 +0800

    feat: 处理异步代码

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,4 +1,5 @@
 // 博客相关方法
+// 获取博客列表数据
 const getList = (author, keyword) => {
   // 从数据库里拿数据
   return [
@@ -18,7 +19,17 @@ const getList = (author, keyword) => {
     }
   ]
 }
-
+// 获取博客详情
+const getDetail = (id) => {
+  return {
+    id: 1,
+    title: '标题1',
+    content: '内容1',
+    author: 'zhangsan',
+    createAt: 1744209483584
+  }
+}
 module.exports = {
-  getList
+  getList,
+  getDetail
 };
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,32 +1,36 @@
 const { SuccessModel } = require("../model/responseModel");
-const { getList } = require('../controllers/blog');
+const { getList, getDetail } = require('../controllers/blog');
 
 // 处理博客相关的路由
 const handleBlogRoute = (req, res) => {
   // 定义处理路由的逻辑
   const method = req.method;
   
-
+  // 博客列表路由
   if (method === 'GET' && req.path === '/api/blog/list') {
     const {author = '', keyword = ''} = req.query;
     const listData = getList(author, keyword);
-    return new SuccessModel(listData, '获取博客列表的接口')
+    return new SuccessModel(listData)
   }
+  // 博客详情路由
   if (method === 'GET' && req.path === '/api/blog/detail') {
-    return {
-      message: '获取博客详情的接口'
-    }
+    const { id } = req.query;
+    const detailData = getDetail(id)
+    return new SuccessModel(detailData);
   }
+  // 新建博客路由
   if (method === 'POST' && req.path === '/api/blog/new') {
     return {
       message: '新建博客的接口'
     }
   }
+  // 更新博客路由
   if (method === 'POST' && req.path === '/api/blog/update') {
     return {
       message: '更新博客的接口'
     }
   }
+  // 删除博客路由
   if (method === 'POST' && req.path === '/api/blog/delete') {
     return {
       message: '删除博客的接口'
